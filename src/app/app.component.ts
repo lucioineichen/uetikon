f13143ff84b22802a01138b40e249328563312e4
lucioineichen <78795594+lucioineichen@users.noreply.github.com>
2024-08-31 19:52:21 +0200

    saufen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'uetikon';
+  sauf = -1;
+  saufen = [
+    'Lüzli',
+    'Pieti',
+    'Yäne',
+    'LX',
+    'Sime',
+    'Schösche',
+    'Pancho',
+    'Timmeli',
+  ];
+
+  changeSauf() {
+    this.sauf = Math.floor(Math.random() * 8);
+  }
 }
